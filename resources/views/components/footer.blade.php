22c9cbbd2265eae99eb0b665778709a52c7c990a
Md.Sakib <rahman.polash188@gmail.com>
2024-04-24 23:11:19 +0600

    portfolio

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="py-4 shadow-sm text-white bg-dark mt-auto">
+    <div class="container d-flex justify-content-center align-items-center h-100">
+        <div class="row">
+            <div class="col text-center">
+                <div class="small">Copyright &copy; Visual Career 2024</div>
+            </div>
+        </div>
+    </div>
+</footer>
